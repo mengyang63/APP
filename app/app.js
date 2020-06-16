7e6fa3756630821342f5a84ed579b5cae2e2a0df
Mengyang <mengyang63@126.com>
2020-06-16 02:17:46 +0000

    Basic Structure

--- app/app.js
+++ app/app.js
@@ -1,20 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-	var campgrounds = [
+var campgrounds = [
 		{name: "Beijing", image: "https://www.nationalgeographic.com/content/dam/travel/Guide-Pages/asia/beijing_travel.jpg" },
 		{name: "Rizhao", image:"sfs"},
 		{name: "Shanghai", image:""}		
 	];
-	
+
+app.get("/campgrounds", function(req, res){
 	res.render("campgrounds", {campgrounds, campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+	var name = req.body.name;
+	var image = req.body.image;
+	var campground = {name:name, image:image};
+	campgrounds.push(campground);
+	// get data from form and add them onto the array
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new.ejs");
 });
 
 app.listen(3000, function() { 
