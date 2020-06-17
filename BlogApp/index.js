1e32914fc4ce6d26dd6a14a5dd9c750490133f78
Mengyang <mengyang63@126.com>
2020-06-17 08:21:09 +0000

    combined with delete and edit

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -1,4 +1,5 @@
 var express = require("express"),
+	methodOverride = require("method-override"),
 	app = express(),
 	mongoose = require("mongoose"),
 	bodyParser = require("body-parser");
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/Restful");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //Mongoose/Model config
 var blogSchema =  new mongoose.Schema({
@@ -42,15 +44,62 @@ app.get("/blogs", function(req, res){
 	}); 
 });
 
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+	Blog.create(req.body.blog, function(err, newBlog){
+		if (err) {
+			res.render("new");
+		} else {
+			res.redirect("/blogs");
+		}
+	})
+});
+
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+	})
+});
 
 
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("edit",{blog: foundBlog});	
+		}
+	})
+});
 
 
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	}); 
+});
 
 
-
-
-
+app.delete("/blogs/:id", function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
 
 
 app.listen(3000,function(){
